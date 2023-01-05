51eb560b38ad16b211fb8a55a8a790eda7783c4a
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-05 20:49:17 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('./src/app');
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
