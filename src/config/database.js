0a43ffa6db1f000c9b18f2dd241cd11209652c23
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-06 02:22:54 +0000

    Adding database connection

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+const {Pool}=require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    
+})
+
+pool.on('connect', ()=>{
+    console.log('Connected to database')
+});
+
+module.exports ={
+    query:(text,params)=>pool.query(text,params),
+};
